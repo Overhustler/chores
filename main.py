3d9e9ae8bab5c956e7b3ddcd6600c5e7460f6d85
Overhustler <joshua.overhulser87@gmail.com>
2026-04-29 02:23:31 -0400

    update main

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from data.choreList  import ChoreList, chore_resident_restriction
-
+from data.choreList  import ChoreList
+import data.resident 
 def main():
     chore_list = ChoreList("male_chores", "choreTest.txt")
-    resident_one_ristriction_list = chore_list.chore_resident_restriction("resident_one_restriction_list", chore_list.getChores(), {})
-    resident_one_ristriction_list
+    resident = chore_list
 
 if __name__ == "__main__":
     main()
